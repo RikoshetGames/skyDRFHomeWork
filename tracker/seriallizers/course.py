ecaddad1c6157b7c95d955c6fbfc9adea85d9eba
RikoshetGames <ilyalew@mail.ru>
2024-02-24 12:09:14 +0300

    DRF HomeWork 24.2

--- tracker/seriallizers/course.py
+++ tracker/seriallizers/course.py
@@ -1,13 +1,17 @@
 from rest_framework import serializers, fields
 
 from tracker.models import Course
+from tracker.seriallizers.lesson import LessonSerializer
 
 
 class CourseSerializer(serializers.ModelSerializer):
     lesson_count = fields.SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True)
+
+    def get_lesson_count(self, obj):
+        return obj.lesson_set.count()
+
     class Meta:
         model = Course
         fields = ['id', 'name', 'description', 'lesson_count']
 
-    def get_lesson_count(self, obj):
-        return obj.lesson_set.count()
