3e1f0d445bf70348101db4224ff36d8be5dc4c55
RikoshetGames <ilyalew@mail.ru>
2024-03-08 22:57:39 +0300

    DRF HomeWork 25.2

--- tracker/seriallizers/course.py
+++ tracker/seriallizers/course.py
@@ -1,15 +1,22 @@
 from rest_framework import serializers, fields
 
-from tracker.models import Course
+from tracker.models import Course, Subscription
 from tracker.seriallizers.lesson import LessonSerializer
 
 
 class CourseSerializer(serializers.ModelSerializer):
     lesson_count = fields.SerializerMethodField()
     lessons = LessonSerializer(many=True, read_only=True)
+    is_subscribed = serializers.SerializerMethodField()
 
     def get_lesson_count(self, obj):
         return obj.lessons.count()
+
+    def get_is_subscribed(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            return Subscription.objects.filter(user=user, course=obj).exists()
+        return False
 
     class Meta:
         model = Course
