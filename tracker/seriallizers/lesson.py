82387bd1fc268b6bac0efe31ca43a2d617006919
RikoshetGames <ilyalew@mail.ru>
2024-03-08 22:47:48 +0300

    DRF HomeWork 25.2

--- tracker/seriallizers/lesson.py
+++ tracker/seriallizers/lesson.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 
 from tracker.models import Lesson
+from tracker.validators import UrlValidator
 
 
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
+        validators = [UrlValidator(field='video_link')]
